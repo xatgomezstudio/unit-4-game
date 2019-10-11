3d3bafb78b93c6ce3a50c6339ed2101327340082
Catherine Gomez <catherinegomezstudio@gmail.com>
2019-10-10 22:35:19 -0400

    functional version

--- 04-crystalgame/assets/javascript/game.js
+++ 04-crystalgame/assets/javascript/game.js
@@ -1,53 +1,82 @@
+
+    var fundsNumber = 1;
+    var debtNumber = 0;
+    var score = 0;
+
 $(document).ready(function () {
-
-    function gameClear() {
-        fundsNumber = 0;
-        debtNumber = 0;
-
-        $("#funds-area").text("$ " + fundsNumber);
-        $("#debt-area").text("$ " + debtNumber);
-    }
-    gameClear();
 
     function gameOn() {
 
 
         function debt() {
 
-                debtNumber = Math.floor(Math.random() * 120000) + 19000;
-                $("#debt-area").text("-$ " + debtNumber);
+            debtNumber = Math.floor(Math.random() * 120000) + 19000;
+            $("#debt-area").text("-$ " + debtNumber);
 
-                console.log("Debt: -$ " + debtNumber);
+            console.log("Debt: -$ " + debtNumber);
         }
         debt();
-
-
-        function button() {
-                buttonNumber = Math.floor(Math.random() * 1200) + 100;
-        }
-        button();
-
-    }    
+    }
 
     $("#start-button").on("click", function () {
         gameOn();
     })
 
+    function button() {
+        var buttonNumber = Math.floor(Math.random() * 12000) + 100;
+        fundsNumber += buttonNumber;
+        checkScore();
+    }
 
-    $(".funds").on("click", function () {
+    function buttonOne() {
+        fundsNumber = fundsNumber + 1;
+        checkScore();
+    }
 
-        var buttonValue = ($(this).attr("data-income"));
-        buttonValue = parseInt(buttonValue);
+    score
 
-        fundsNumber += buttonValue
 
-        if (fundsNumber === debtNumber) {
-            alert("Winner winner, chicken dinner!");
-        }
-        else if (fundsNumber > debtNumber) {
-            alert("You snooze, you lose!");
-        }
+    $("#savings").on("click", function () {
+        button();
+        $("#funds-area").text(fundsNumber);
+    })
+    $("#job").on("click", function () {
+        button();
+        $("#funds-area").text(fundsNumber);
+    })
+    $("#sideHustle").on("click", function () {
+        buttonOne()
+        $("#funds-area").text(fundsNumber);
     })
 
 })
 
+
+
+function checkScore() {
+
+    if (fundsNumber === debtNumber) {
+        $(document).ready(function () {
+          
+            alert("ya win")
+        });
+        function scoreAdd() {
+            ++score
+            $("win-area").text(score);
+        }
+        gameClear();
+        scoreAdd();
+    }
+    else if (fundsNumber > debtNumber) {
+        alert("You snooze, you lose!");
+        
+    }
+   // gameClear();
+}
+function gameClear() {
+    fundsNumber = 0;
+    debtNumber = 0;
+
+    $("#funds-area").text("$ " + fundsNumber);
+    $("#debt-area").text("$ " + debtNumber);
+}
